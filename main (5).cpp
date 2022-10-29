78bcb2d088d4fafec06a91abbdd861a9d47cb587
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-10-29 09:50:39 +0530

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class node {
+public:
+  int data;
+  node *left;
+  node *right;
+  node(int d) {
+    this->data = d;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+node *BuildTree(node *root) {
+  cout << "enter data" << endl;
+  int data;
+  cin >> data;
+  root = new node(data);
+  if (data == -1)
+    return NULL;
+
+  cout << "enter data to be inserted in the left of " << data << endl;
+  root->left = BuildTree(root->left);
+  cout << "enter data to be inserted in the right of " << data << endl;
+  root->right = BuildTree(root->right);
+  return root;
+}
+void levelordertransversal(node *root) {
+  queue<node *> q;
+  q.push(root);
+  q.push(NULL); // NULL acts as a separator
+  while (!q.empty()) {
+    node *temp = q.front();
+    q.pop();
+
+    if (temp == NULL) {
+      cout << endl;
+      if (!q.empty())
+        q.push(NULL);
+    } else {
+      cout << temp->data << " ";
+      if (temp->left)
+        q.push(temp->left);
+      if (temp->right)
+        q.push(temp->right);
+    }
+  }
+}
+void inorder(node *root, int &cnt) {
+  if (root == NULL)
+    return;
+  inorder(root->left, cnt);
+  // leaf node
+  if (root->left == NULL && root->right == NULL)
+    cnt++;
+  inorder(root->right, cnt);
+}
+int main() {
+  //  1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+  node *root = NULL;
+  root = BuildTree(root);
+  int cnt = 0;
+  inorder(root, cnt);
+  cout << "Number of leaf nodes:" << cnt;
+  // levelordertransversal(root);
+}
