c821929010987d2d7110632d87aa278baa2561e9
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-10-29 23:03:27 +0530

    Update and rename main (6).cpp to Height.cpp

---
+++ Height.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int height(node* root){
+  //base case
+  if(root==NULL)
+    return 0;
+  int left=height(root->left);
+  int right=height(root->right);
+  int ans=max(left,right)+1;
+  return ans;
+}
