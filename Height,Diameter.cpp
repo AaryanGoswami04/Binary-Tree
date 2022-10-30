45eb1da40caeab84b9b92a848381fe41d243bf70
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-10-30 10:23:22 +0530

    Update and rename Height.cpp to Height,Diameter.cpp

---
+++ Height,Diameter.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int height(node* root){
+  //base case
+  if(root==NULL)
+    return 0;
+  int left=height(root->left);
+  int right=height(root->right);
+  int ans=max(left,right)+1;
+  return ans;
+}
+int diameter(node*root){   //Brute Force Time complexity:O(N^2) Each node is being traversed twice
+  //base case
+  if(root==NULL)
+    return 0;
+  int op1=diameter(root->left);
+  int op2=diameter(root->right);
+  int op3=height(root->left)+1+height(root->right);
+  int ans=max(op1,max(op2,op3));
+  return ans;
+}
+pair<int,int> fastdiameter(node*root){   //Optimised approach:O(N) Each node is being transversed only once
+  //base case
+  if(root==NULL)
+    return {0,0};
+  pair<int,int>left=fastdiameter(root->left);
+   pair<int,int>right=fastdiameter(root->right);
+
+  int op1=left.first;
+  int op2=right.first;
+  int op3=left.second+right.second+1;
+  
+  pair<int,int>ans;
+  ans.first=max(op1,max(op2,op3));
+  ans.second=max(left.second,right.second)+1;
+
+  return ans;
+  }
