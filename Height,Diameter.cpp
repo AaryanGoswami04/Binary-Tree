4030288c002b00cb829afa4a4164e9bfbddf0d63
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-10-30 10:49:53 +0530

    Update Height,Diameter.cpp

--- Height,Diameter.cpp
+++ Height,Diameter.cpp
@@ -10,7 +10,7 @@ int height(node* root){
   int ans=max(left,right)+1;
   return ans;
 }
-int diameter(node*root){   //Brute Force Time complexity:O(N^2) Each node is being traversed twice
+int diameter(node*root){   //Brute Force Time complexity:O(N^2)
   //base case
   if(root==NULL)
     return 0;
@@ -27,13 +27,13 @@ pair<int,int> fastdiameter(node*root){   //Optimised approach:O(N) Each node is 
   pair<int,int>left=fastdiameter(root->left);
    pair<int,int>right=fastdiameter(root->right);
 
-  int op1=left.first;
-  int op2=right.first;
-  int op3=left.second+right.second+1;
+  int op1=left.first;   //stores diameter of left subtree
+  int op2=right.first; //stores diameter of right subtree
+  int op3=left.second+right.second+1; //stores sum of left subtree's height+right subtree's height+1
   
   pair<int,int>ans;
-  ans.first=max(op1,max(op2,op3));
-  ans.second=max(left.second,right.second)+1;
+  ans.first=max(op1,max(op2,op3));    //stores diameter of the tree
+  ans.second=max(left.second,right.second)+1; //stores height of the tree
 
   return ans;
   }
