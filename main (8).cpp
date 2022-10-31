9f9c9039f88d84de545ec1b912aee60f44e89305
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-10-31 22:06:37 +0530

    Update main (8).cpp

--- main (8).cpp
+++ main (8).cpp
@@ -1,52 +1,5 @@
 #include <iostream>
 #include <queue>
-using namespace std;
-class node {
-public:
-  int data;
-  node *left;
-  node *right;
-  node(int d) {
-    this->data = d;
-    this->left = NULL;
-    this->right = NULL;
-  }
-};
-node *BuildTree(node *root) {
-  cout << "enter data" << endl;
-  int data;
-  cin >> data;
-  root = new node(data);
-  if (data == -1)
-    return NULL;
-
-  cout << "enter data to be inserted in the left of " << data << endl;
-  root->left = BuildTree(root->left);
-  cout << "enter data to be inserted in the right of " << data << endl;
-  root->right = BuildTree(root->right);
-  return root;
-}
-void levelordertransversal(node *root) {
-  queue<node *> q;
-  q.push(root);
-  q.push(NULL); // NULL acts as a separator
-  while (!q.empty()) {
-    node *temp = q.front();
-    q.pop();
-
-    if (temp == NULL) {
-      cout << endl;
-      if (!q.empty())
-        q.push(NULL);
-    } else {
-      cout << temp->data << " ";
-      if (temp->left)
-        q.push(temp->left);
-      if (temp->right)
-        q.push(temp->right);
-    }
-  }
-}
 int height(node *root) {
   // base case
   if (root == NULL)
@@ -56,25 +9,39 @@ int height(node *root) {
   int ans = max(left, right) + 1;
   return ans;
 }
- bool isBalanced(node *root)
+ bool isBalanced(node *root)  //Time Complexity:O(N^2)
     {
         //base code
         if(root==NULL)
-        return true;
+         return true;
         
         bool left=isBalanced(root->left);
-         bool right=isBalanced(root->right);
+        bool right=isBalanced(root->right);
          
-         bool diff=abs(height(root->left)-height(root->right))<=1;
-         if(left && right && diff)
-         return 1;
-         else
-         return 0;
+        bool diff=abs(height(root->left)-height(root->right))<=1;
+        if(left && right && diff)
+          return 1;
+        else
+          return 0;
     }
-int main() {
-  //  1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-  // 1 5 2 -1 -1 6 7 -1 -1 -1 10 15 8 -1 -1 -1 -1
-  node *root = NULL;
-  root = BuildTree(root);
-  cout << isBalanced(root)<< endl;
+pair<bool,int> isBalancedFast(node* root){   //Optimised approach:TC->O(N)
+   //base case
+   if(root==NULL)
+     return {true,0};
+  
+  pair<int,int>left=isBalancedFast(root->left);
+  pair<int,int>right=isBalancedFast(root->right);
+  
+  bool LeftisBalanced=left.first;
+  bool RightisBalanced=right.first;
+  bool diff=abs(left.second-left.second)<=1;   //difference of their heights
+  
+  pair<bool,int>ans;
+  ans.second=max(left.second,right.second)+1;
+  
+  if(LeftisBalanced && RightisBalanced && diff)
+    ans.first=true;
+  else
+    ans.first=false;
+  return ans;
 }
