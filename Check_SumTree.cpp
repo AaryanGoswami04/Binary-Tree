b9ff16190cf9201e9383350fed67ed6eb07cc4a7
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-01 10:25:35 +0530

    Update and rename main (10).cpp to Check_SumTree.cpp

--- Check_SumTree.cpp
+++ Check_SumTree.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
-int main() {
-  pair<bool,int> isSumTreeFast(Node*root){
-          //base case
+ /*Given a Binary Tree. Return true if, for every node X in the tree other than the leaves, its value is equal to the sum of its left subtree's value and its right subtree's value. Else return false.
+An empty tree is also a Sum Tree as the sum of an empty tree can be considered to be 0. A leaf node is also considered a Sum Tree.*/
+pair<bool,int> isSumTreeFast(Node*root){
+          //base cases
         if(root==NULL)
-        return {true,0};
+           return {true,0};
         
         if(root->left==NULL && root->right==NULL)//leaf node
-        return {true,root->data};
+           return {true,root->data};
         
         pair<bool,int>leftAns=isSumTreeFast(root->left);
         pair<bool,int>rightAns=isSumTreeFast(root->right);
@@ -23,7 +24,7 @@ int main() {
             ans.second=2*root->data;
         }
         else
-        ans.first=false;
+            ans.first=false;
         return ans;
         
     }
