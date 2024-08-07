88fe9c35807a410ba9f88dd1d91bdd432b972636
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2024-08-07 12:01:55 +0530

    Update BottomView.cpp

--- BottomView.cpp
+++ BottomView.cpp
@@ -10,35 +10,38 @@ Note: If there are multiple bottom-most nodes for a horizontal distance from the
        10      14
 
 For the above tree, the output should be 5 10 4 14 25.*/
-//TC: O(N log N); SC:O(N)
+//TC: O(N); SC:O(N)
 //Print all nodes present at each x coordinate and are at max height from left to right 
-    vector <int> bottomView(Node *root) {
+vector <int> bottomView(Node *root) {
         vector <int>ans;
-        map<int, pair<int, Node*>> mp; //Stores {x, {y,Node}} 
-        queue<pair<Node*, vector<int>>> q;  //Stores {Node, {x,y}} 
+        unordered_map<int, Node*> mp; //Stores {x, Node}
+        queue<pair<Node*, int>> q;  //Stores {Node, x} 
+        int minx=0, maxx=0; 
         
-        q.push({root, {0,0}}); //root is at (0,0) (Reference)
-        mp[0] = {0,root}; 
+        q.push({root, 0}); //root is at (0,0) (Reference)
+        mp[0] = root; 
         
         while(q.size()){
             Node* front = q.front().first;
-            int x = q.front().second[0], height = q.front().second[1];
+            int x = q.front().second;
+            
+            minx =min(minx, x);
+            maxx = max(maxx, x);
+            
             q.pop();
             
-            //Hash x coordinate to correspondind {y,Node}
+            //Hash x coordinate to correspondind Node
             if(front->left){
-                q.push({front->left, {x-1, height+1}});
-                mp[x-1] = {height+1, front->left};
+                q.push({front->left, x-1});
+                mp[x-1] = front->left;
             }
             if(front->right){
-                q.push({front->right, {x+1, height+1}});
-                mp[x+1] = {height+1, front->right};
+                q.push({front->right, x+1});
+                mp[x+1] = front->right;
             }
         }
         
-        for(auto itr:mp){
-             ans.push_back(itr.second.second->data);
-        }
+        for(int i=minx; i<=maxx; i++) ans.push_back(mp[i]->data);
         
         return ans;
-    }
+}
